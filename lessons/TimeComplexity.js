531970d93203c89a13a4b7b3a92b37c141f73839
Bruno Agutoli <bruno.agutoli@gmail.com>
2017-01-16 20:32:54 -0200

    added PermMissingElem algorithms

--- lessons/TimeComplexity.js
+++ lessons/TimeComplexity.js
@@ -6,3 +6,14 @@ export const FrogJmp = (X, Y, D) => {
   }
   return jump;
 }
+
+export const PermMissingElem = (A) => {
+  A.sort((a, b) => parseInt(a) - parseInt(b));
+  for(let i = 0; i < A.length + 1; i++) {
+    let index = i + 1;
+    if (index !== A[i]) {
+      return index;
+    }
+  }
+  return 1;
+}
--- lessons/TimeComplexity.spec.js
+++ lessons/TimeComplexity.spec.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 
-import { FrogJmp } from './TimeComplexity';
+import { FrogJmp, PermMissingElem } from './TimeComplexity';
 
 describe('Time Complexity', () => {
   describe('FrogJmp', () => {
@@ -16,6 +16,28 @@ describe('Time Complexity', () => {
       let Y = 999111321;
       let D = 7;
       assert.deepEqual(FrogJmp(X, Y, D), 142730189);
+    });
+  });
+
+  describe('PermMissingElem', () => {
+    it('should return 4', () => {
+      assert.equal(PermMissingElem([2, 3, 1, 5]), 4);
+    });
+
+    it('should return 1', () => {
+      assert.equal(PermMissingElem([2]), 1);
+    });
+
+    it('should return 2', () => {
+      assert.equal(PermMissingElem([1, 1, 3]), 2);
+    });
+
+    it('empty list', () => {
+      assert.equal(PermMissingElem([]), 1);
+    });
+
+    it('only one item', () => {
+      assert.equal(PermMissingElem([1]), 2);
     });
   });
 });
